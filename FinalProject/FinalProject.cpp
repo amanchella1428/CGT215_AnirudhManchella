79edf514cdc64596a1e2e53f28c0f67391e14540
amanchella1428 <animanchella@gmail.com>
2024-11-26 15:58:04 -0500

    Updating final project

--- FinalProject/FinalProject.cpp
+++ FinalProject/FinalProject.cpp
@@ -1,60 +1,68 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <vector>
-#include <string>
-
-using namespace sf;
 using namespace std;
 
-//A custom clamp function because I don't know how to update to c++ 18 or higher
-template <typename T>
-T clamp(T value, T min, T max) {
-    return (value < min) ? min : (value > max) ? max : value;
+//sharpness modifier for me to debug
+std::vector<std::vector<int>> createSharpenKernel(int sharpenStrength) {
+    return {
+        {  0, -sharpenStrength,  0},
+        { -sharpenStrength,  4 + sharpenStrength, -sharpenStrength},
+        {  0, -sharpenStrength,  0}
+    };
 }
 
-//Using Kernals to get the image itself
-Image applyConvolution(const Image& image, const vector<vector<int>>& kernel) {
-    Vector2u sz = image.getSize();
-    unsigned int width = sz.x, height = sz.y;
-
+//applying the first convolution: sharpness
+sf::Image applyConvolution(const sf::Image& image, const std::vector<std::vector<int>>& kernel) {
+    int width = image.getSize().x;
+    int height = image.getSize().y;
     int kernelSize = kernel.size();
     int kernelRadius = kernelSize / 2;
 
-    //Creating a new image for the final result.
-    Image result;
+    sf::Image result;
     result.create(width, height);
 
-    //Iterating over each pixel in a nested for loop for rows and columns
-    for (unsigned int y = 0; y < height; ++y) {
-        for (unsigned int x = 0; x < width; ++x) {
-            int newValue = 0;
+    //Going through each pixel
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+            int redValue = 0;
+            int greenValue = 0;
+            int blueValue = 0;
 
-            //Applying the kernals (I think my math is correct since I learned about this in MA 265 and 266)
+            //Applies the kernerls for the color change
             for (int ky = -kernelRadius; ky <= kernelRadius; ++ky) {
                 for (int kx = -kernelRadius; kx <= kernelRadius; ++kx) {
-                    int pixelX = x + kx;
                     int pixelY = y + ky;
+                    int pixelX = x + kx;
 
-                    //Checking the boundaries and applying color
-                    if (pixelX >= 0 && pixelX < static_cast<int>(width) && pixelY >= 0 && pixelY < static_cast<int>(height)) {
-                        Color pixelColor = image.getPixel(pixelX, pixelY);
-                        int gray = pixelColor.r; //Assuming grayscale (RGB stuff)
-                        newValue += gray * kernel[ky + kernelRadius][kx + kernelRadius];
+                    //Checks for the boundaries
+                    if (pixelY >= 0 && pixelY < height && pixelX >= 0 && pixelX < width) {
+                        sf::Color pixel = image.getPixel(pixelX, pixelY);
+
+                        //Applies to kernels to each color channel
+                        redValue += pixel.r * kernel[ky + kernelRadius][kx + kernelRadius];
+                        greenValue += pixel.g * kernel[ky + kernelRadius][kx + kernelRadius];
+                        blueValue += pixel.b * kernel[ky + kernelRadius][kx + kernelRadius];
                     }
                 }
             }
 
-            //Calling the custom clamp function
-            newValue = clamp(newValue, 0, 255);
-            result.setPixel(x, y, Color(newValue, newValue, newValue)); //eliminating the greyscale
+            //Clamp the color values to be within the range of [0, 255]
+            redValue = std::min(std::max(redValue, 0), 255);
+            greenValue = std::min(std::max(greenValue, 0), 255);
+            blueValue = std::min(std::max(blueValue, 0), 255);
+
+            //Set the new pixel value in the result image
+            result.setPixel(x, y, sf::Color(redValue, greenValue, blueValue));
         }
     }
 
     return result;
 }
 
+
 int main() {
-    string imagePath = "C:/Users/Owner/source/repos/amanchella1428/CGT215_AnirudhManchella/FinalProject/Images/backgrounds/winter.png"; //fails to load without absolute path for some reason
+    string imagePath = "C:/Users/Owner/source/repos/amanchella1428/CGT215_AnirudhManchella/FinalProject/Images/backgrounds/winter.png"; //Doesn't work unless I put the whole thing for some reason
 
     sf::Texture texture;
     if (!texture.loadFromFile(imagePath)) {
@@ -62,18 +70,19 @@ int main() {
         return -1;
     }
 
-    sf::RenderWindow window(sf::VideoMode(1024, 768), "Scaled Image Display"); //Fixed window size to compare images, we can make this custom later with variables
+    sf::Image image = texture.copyToImage();
+
+    //Adjusting the stregth here (dubug purposes)
+    int sharpenStrength = 1;
+    auto sharpenKernel = createSharpenKernel(sharpenStrength);
+    sf::Image sharpenedImage = applyConvolution(image, sharpenKernel);
+
+    sf::RenderWindow window(sf::VideoMode(1024, 768), "Sharpened Image Display");
 
     sf::Sprite sprite;
-    sprite.setTexture(texture);
-
-    //scaling the sprite to fit into the window (stretches the image if we expand it out)
-    sf::Vector2u imageSize = texture.getSize();
-    sf::Vector2u windowSize = window.getSize();
-    float scaleX = static_cast<float>(windowSize.x) / imageSize.x;
-    float scaleY = static_cast<float>(windowSize.y) / imageSize.y;
-    float scale = std::min(scaleX, scaleY);
-    sprite.setScale(scale, scale);
+    sf::Texture sharpenedTexture;
+    sharpenedTexture.loadFromImage(sharpenedImage);
+    sprite.setTexture(sharpenedTexture);
 
     while (window.isOpen()) {
         sf::Event event;
@@ -89,5 +98,3 @@ int main() {
 
     return 0;
 }
-
-//This was really hard please go easy on my grade
